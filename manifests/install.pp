0fd52999692f62540961e5d9584127fc716231f9
Yehuda Katz <yehuda@ymkatz.net>
2020-10-29 23:53:19 -0400

    Fix package conflicts

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,21 +8,26 @@ class csf::install inherits csf {
     }
   }
 
-  package { 'iptables':
-    ensure => installed,
-    before => Exec['csf-install'],
+  if ! defined(Package['iptables']) {
+    package { 'iptables':
+      ensure => installed,
+      before => Exec['csf-install'],
+    }
   }
 
-  package { 'csf-perl':
-    ensure => installed,
-    name   => 'perl',
+  if ! defined(Package['perl']) {
+    package { 'perl':
+      ensure => installed,
+      name   => 'perl',
+    }
   }
-  -> exec { 'csf-install':
+
+  exec { 'csf-install':
     cwd     => '/tmp',
     command => "/usr/bin/curl -o csf.tgz ${::csf::download_location} && tar -xzf csf.tgz && cd csf && sh install.sh",
     creates => '/usr/sbin/csf',
     notify  => Service['csf'],
-    require => Package['csf-perl'],
+    require => Package['perl'],
   }
 
   # make sure testing is disabled, we trust puppet enough
